c5fc226ef969fd33e0be3817374ce50f97524ff9
awwq8293-pixel <186656368+AryancYs33@users.noreply.github.com>
2025-11-21 01:53:23 -0500

    codigo crud repository

--- src/repository/CrudRepository.java
+++ src/repository/CrudRepository.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package repository;
 
-/**
- *
- * @author Personal
- */
-public class CrudRepository {
+public interface CrudRepository<T, ID> extends Repository<T, ID> {
     
 }
