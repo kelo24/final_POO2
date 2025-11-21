3d6c0d23e9d651d60dfb2ef777935835b8a5b968
awwq8293-pixel <186656368+AryancYs33@users.noreply.github.com>
2025-11-21 01:51:39 -0500

    repository primera part de codigo

--- src/repository/Repository.java
+++ src/repository/Repository.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package repository;
 
-/**
- *
- * @author Personal
- */
-public class Repository {
-    
-}
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository<T, ID> {
+
+    void guardar(T entidad);
+
+    void actualizar(T entidad);
+
+    void eliminar(ID id);
